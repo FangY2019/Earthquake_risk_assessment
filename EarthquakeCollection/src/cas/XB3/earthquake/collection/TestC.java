4fe05d2ca84efda6238c1f0356459428111c7d3d
Kalindu Sembakutti <sembukkk@mcmaster.ca>
2020-03-01 16:41:43 -0500

    earth

--- EarthquakeCollection/src/cas/XB3/earthquake/collection/TestC.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/collection/TestC.java
@@ -2,16 +2,16 @@ package cas.XB3.earthquake.collection;
 
 public class TestC {
     public static void main(String[] args){
-        MarbleBag<Marble> bag = new MarbleBag<Marble>();
+        EarthquakeBag <EarthquakeT> bag = new EarthquakeBag<EarthquakeT>();
 
-        Marble m1 = new Marble("Cat", Marble.Color.BLUE, 2.2);
-        Marble m2 = new Marble("Dog", Marble.Color.BLACK, 3.4);
-        Marble m3 = new Marble("Rat", Marble.Color.BLUE, 2.0);
-        Marble m4 = new Marble("Bird", Marble.Color.BLUE, 2.5);
-        Marble m5 = new Marble("Snake", Marble.Color.PINK, 3.4);
-        Marble m6 = new Marble("Ant", Marble.Color.WHITE, 1.4);
-        Marble m7 = new Marble("Elephant", Marble.Color.WHITE, 6.0);
-        Marble m8 = new Marble("Bird", Marble.Color.BLUE, 2.5);
+        EarthquakeT m1 = new EarthquakeT("Cat", EarthquakeT.ColorRating.BLUE, 2.2);
+        EarthquakeT m2 = new EarthquakeT("Dog", EarthquakeT.ColorRating.GREEN, 3.4);
+        EarthquakeT m3 = new EarthquakeT("Rat", EarthquakeT.ColorRating.BLUE, 2.0);
+        EarthquakeT m4 = new EarthquakeT("Bird", EarthquakeT.ColorRating.BLUE, 2.5);
+        EarthquakeT m5 = new EarthquakeT("Snake", EarthquakeT.ColorRating.BLUE, 3.4);
+        EarthquakeT m6 = new EarthquakeT("Ant", EarthquakeT.ColorRating.BLUE, 1.4);
+        EarthquakeT m7 = new EarthquakeT("Elephant", EarthquakeT.ColorRating.BLUE, 6.0);
+        EarthquakeT m8 = new EarthquakeT("Bird", EarthquakeT.ColorRating.BLUE, 2.5);
 
         bag.add(m1);
         bag.add(m2);
@@ -24,15 +24,7 @@ public class TestC {
 
         System.out.println(bag.size());
 
-        bag.del(m8);
-
-        System.out.println(bag.size());
-
-        System.out.println(bag.sizeC(Marble.Color.BLUE));
-
-        bag.delc(Marble.Color.BLUE);
-
-        System.out.println(bag.sizeC(Marble.Color.BLUE));
+        System.out.println(bag.sizeC(EarthquakeT.ColorRating.BLUE));
 
     }
 }
