2db9c05d67668638a32fd36f73ef04b6194b4b7f
Fang Ye <yef10@mcmaster.ca>
2020-03-03 21:51:41 -0500

    add PointT ADT

---
+++ EarthquakeCollection/src/cas/XB3/earthquake/ADT/PointT.java
@@ -0,0 +1,18 @@
+package cas.XB3.earthquake.ADT;
+
+public class PointT {
+	private double x;
+	private double y;
+	
+	public PointT(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distanceTo(PointT that) {
+		double distance = 0.0;
+		
+		return distance;
+	}
+
+}
