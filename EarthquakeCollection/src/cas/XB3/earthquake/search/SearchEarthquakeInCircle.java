62b03191ee67529b7e07003245bc6b034e2ca8f1
Fang Ye <yef10@mcmaster.ca>
2020-03-05 21:22:13 -0500

    update SearchEarthquakeInCircle module

--- EarthquakeCollection/src/cas/XB3/earthquake/search/SearchEarthquakeInCircle.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/search/SearchEarthquakeInCircle.java
@@ -2,13 +2,17 @@ package cas.XB3.earthquake.search;
 
 import java.util.ArrayList;
 
+import cas.XB3.earthquake.ADT.PointT;
+import cas.XB3.earthquake.collection.CSVreader;
+import cas.XB3.earthquake.collection.EarthquakeBag;
+import cas.XB3.earthquake.collection.EarthquakeT;
+
 public class SearchEarthquakeInCircle {
 	private ArrayList<EarthquakeT> earthquakeList = new ArrayList<>();
 	
 	//load the earthquake database
 	EarthquakeBag <EarthquakeT> Earthquakebag = new EarthquakeBag<EarthquakeT>();
-	CSVreader.readEarthquakes("./eqarchive-en.csv", bag);
-	
+	//readEarthquakes("./eqarchive-en.csv", Earthquakebag);	
 	
 	//get the list of earthquake which are in the circle of the given PointT and radius
 	public ArrayList<EarthquakeT> searchEarthquakeInCircle(PointT location, double radius){
