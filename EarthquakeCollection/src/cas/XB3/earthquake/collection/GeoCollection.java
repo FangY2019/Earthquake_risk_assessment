87d9114de6820e0ffe3dcafee013963f6359e4b8
SpicaE <taoh4@mcmaster.ca>
2020-03-12 15:48:03 -0400

    update HashMap in GeoCollection

--- EarthquakeCollection/src/cas/XB3/earthquake/collection/GeoCollection.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/collection/GeoCollection.java
@@ -8,16 +8,17 @@ import java.util.HashMap;
 public class GeoCollection {
 	private HashMap<String, ArrayList<CityT>> cityHashMap = new HashMap<>();
 
-    public void add(String province, CityT city){
+    public void add(CityT city){
     	ArrayList<CityT> temp = new ArrayList<>();
-    	if(cityHashMap.get(province) == null) {
+    	String firstCityLetter = getFirstCityLetter(city);
+    	if(cityHashMap.get(firstCityLetter) == null) {
     		temp.add(city);
-    		cityHashMap.put(province, temp);
+    		cityHashMap.put(firstCityLetter, temp);
     	}else {
-    		temp = (ArrayList<CityT>) cityHashMap.get(province).clone();
+    		temp = (ArrayList<CityT>) cityHashMap.get(firstCityLetter).clone();
     		temp.remove(city); // avoid potential duplicated cites
     		temp.add(city);
-    		cityHashMap.put(province, temp);
+    		cityHashMap.put(firstCityLetter, temp);
     	}
     }
 
@@ -31,5 +32,9 @@ public class GeoCollection {
     
     public HashMap<String, ArrayList<CityT>> getCityHashMap(){
     	return cityHashMap;
+    }
+    
+    private String getFirstCityLetter(CityT city) {
+    	return city.getCityName().substring(0,1);
     }
 }
