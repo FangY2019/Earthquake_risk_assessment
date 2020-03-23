08cc1df21e6310ef6be9d410b66b84a73a17c709
Fang Ye <yef10@mcmaster.ca>
2020-03-23 01:32:24 -0400

    modify the print format

--- EarthquakeCollection/src/cas/XB3/earthquake/display/DispalyByMagnitude.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/display/DispalyByMagnitude.java
@@ -1,21 +1,22 @@
 package cas.XB3.earthquake.display;
+
 import java.util.ArrayList;
 
 import cas.XB3.earthquake.ADT.PointT;
 import cas.XB3.earthquake.collection.EarthquakeT;
 import cas.XB3.earthquake.sort.Sort;
 
+public class DispalyByMagnitude implements DisplayInterface {
 
-public class DispalyByMagnitude implements DisplayInterface{
-	
-	public void display(ArrayList<EarthquakeT> earthquakeList, PointT location) {		
+	public void display(ArrayList<EarthquakeT> earthquakeList, PointT location) {
 		Sort.sortByMagnitude(earthquakeList);
-		if (earthquakeList.size() == 0) System.out.println("There's no earthquake!");
+		if (earthquakeList.size() == 0)
+			System.out.println("There's no earthquake!");
 		else {
-		System.out.println("Magnitude         Date                 City");
-		for(EarthquakeT eq: earthquakeList) {
-			System.out.println("" + eq.getMag() +", "+ eq.getDate().getYear() +", " +eq.getPlace());
-		}	
+			System.out.printf("%-15s%-15s%-40s\n", "Magnitude","Date","City");
+			for (EarthquakeT eq : earthquakeList) {
+				System.out.printf("%-15.1f%-15d%-40s\n", eq.getMag(), eq.getDate().getYear(), eq.getPlace());
+			}
 		}
 	}
 
--- EarthquakeCollection/src/cas/XB3/earthquake/display/DisplayByDistance.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/display/DisplayByDistance.java
@@ -6,22 +6,21 @@ import cas.XB3.earthquake.ADT.PointT;
 import cas.XB3.earthquake.collection.EarthquakeT;
 import cas.XB3.earthquake.sort.Sort;
 
-
 public class DisplayByDistance implements DisplayInterface {
 
 	@Override
 	public void display(ArrayList<EarthquakeT> earthquakeList, PointT location) {
 		Sort.sortByDistance(location, earthquakeList);
-		if (earthquakeList.size() == 0) System.out.println("There's no earthquake!");
+		if (earthquakeList.size() == 0)
+			System.out.println("There's no earthquake!");
 		else {
-		System.out.println("Distance     Magnitude         Date                 City");
-		for(EarthquakeT eq: earthquakeList) {
-			System.out.println("" + Math.round(location.distanceTo(eq.getPointT())* 10)/10.0 +", "+ eq.getMag() +", "+ eq.getDate().getYear() +", " +eq.getPlace());
-		}	
+			System.out.printf("%-15s%-15s%-15s%-30s\n","Distance","Magnitude", "Date", "City");
+			for (EarthquakeT eq : earthquakeList) {
+				System.out.printf("%-15.1f%-15.1f%-15d%-30s\n", location.distanceTo(eq.getPointT()), eq.getMag()
+						, eq.getDate().getYear(), eq.getPlace());
+			}
 		}
-		
+
 	}
-	
-	
 
 }
