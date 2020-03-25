1faf0674ab3a5b17630a8b2779abfe0419d1135a
Fang Ye <yef10@mcmaster.ca>
2020-03-25 00:50:02 -0400

    search from RedBlackBST in SearchEarthquakes

--- EarthquakeCollection/src/cas/XB3/earthquake/search/SearchEarthquakes.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/search/SearchEarthquakes.java
@@ -6,20 +6,27 @@ import cas.XB3.earthquake.ADT.PointT;
 import cas.XB3.earthquake.collection.CSVreader;
 import cas.XB3.earthquake.collection.EarthquakeBag;
 import cas.XB3.earthquake.collection.EarthquakeT;
+import cas.XB3.earthquake.collection.RedBlackBST;
 
 public class SearchEarthquakes {
-	
-	//get the list of earthquake which are in the circle of the given PointT and radius
-	public static ArrayList<EarthquakeT> searchEarthquakeInCircle(EarthquakeBag <EarthquakeT> Earthquakebag,PointT location, double radius){
+
+	// get the list of earthquake which are in the circle of the given PointT and
+	// radius
+	public static ArrayList<EarthquakeT> searchEarthquakeInCircle(RedBlackBST<Double, EarthquakeT> bst,
+			PointT location, double radius) {
 
 		ArrayList<EarthquakeT> earthquakeList = new ArrayList<>();
-		for(EarthquakeT earthquake: Earthquakebag) {
-			if(location.distanceTo(earthquake.getPointT()) <= radius) {
-				earthquakeList.add(earthquake);
-			}			
+		// the range of latitude for the given point and radius
+		double[] latRange = location.latFilter(radius);
+		// gets the keys between min latitude and max latitude and gets the earthquakeT according the keys
+		for (Double latitude : bst.keys(latRange[0], latRange[1])) {
+			for (EarthquakeT earthquake : bst.get(latitude)) {
+				if (location.distanceTo(earthquake.getPointT()) <= radius) {
+					earthquakeList.add(earthquake);
+				}
+			}
 		}
 		return earthquakeList;
 	}
-	
-	
+
 }
