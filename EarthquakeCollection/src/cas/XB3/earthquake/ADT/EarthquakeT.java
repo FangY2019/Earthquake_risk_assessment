de9cdb6eb9d6b576177fc3cb4ca06e362d6610df
SpicaE <taoh4@mcmaster.ca>
2020-04-02 21:48:58 -0400

    All ADT classes are now documented

--- EarthquakeCollection/src/cas/XB3/earthquake/ADT/EarthquakeT.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/ADT/EarthquakeT.java
@@ -13,6 +13,18 @@ public class EarthquakeT implements Comparable<EarthquakeT>{
     private ColorRating color;
     private String nameOfProv;
 
+    /**
+     * Constructor of EarthquakeT ADT.
+     * @param place String, the city name where the earthquake happens.
+     * @param prov String, the province name where the earthquake happens.
+     * @param date LocalDateTime, the date of the earthquake.
+     * @param lat double, the latitude of the earthquake.
+     * @param lng double, the longitude of the earthquake.
+     * @param dph double, the depth of the earthquake.
+     * @param mag double, the magnitude of the earthquake.
+     * @param mgT Enum MagType, the type of the earthquake.
+     * @param color Enum ColorRating, the color rating of the earthquake.
+     */
     public EarthquakeT(String place, String prov, LocalDateTime date, double lat, double lng, double dph, double mag, MagType mgT, ColorRating color){
     	this.lat = lat;
     	this.lng = lng;
@@ -25,44 +37,90 @@ public class EarthquakeT implements Comparable<EarthquakeT>{
         this.color = color;
     }
 
+    /**
+     * 
+     * @return String, the name of the province.
+     */
     public String getNameOfProv(){
         return nameOfProv;
     }
 
+    /**
+     * 
+     * @return String, the name of the city.
+     */
     public String getPlace() {
         return place;
     }
 
+    /**
+     * 
+     * @return PointT object, the exact location of the earthquake.
+     */
     public PointT getPointT() {
     	return new PointT(lat, lng);
     }
 
+    /**
+     * 
+     * @return double, the magnitude of the earthquake.
+     */
     public double getMag() {
         return mag;
     }
     
+    /**
+     * 
+     * @return double, the depth of the earthquake.
+     */
     public double getDph() {
     	return dph;
     }
     
+    /**
+     * 
+     * @return Enum MagType, the type of the earthquake.
+     */
     public MagType getMagitudeType() {
     	return magnitudeType;
     }
     
+    /**
+     * 
+     * @return LocalDateTime, the date of the earthquake.
+     */
     public LocalDateTime getDate() {
     	return date;
     }
 
+    /**
+     * 
+     * @return Enum ColorRating, the color rating of the earthquake.
+     */
     public ColorRating getColor(){
         return color;
     }
     
+    /**
+     * @details This method will compare two EarthquakeT object based on their
+     * magnitude.
+     * @return Integer, 1 if the current object have larger magnitude, -1 if
+     * the current object have smaller magnitude, 0 if the two EarthquakeT
+     * object have the same magnitude.
+     */
     public int compareTo(EarthquakeT eq) {
     	if(this.getMag() < eq.getMag()) return -1;
     	else if(this.getMag() > eq.getMag()) return 1;
     	else return 0;
     }
     
+    /**
+     * @details this method will determine if two EarthquakeT objects are
+     * considered equal based on their magnitude, date, place, location, depth,
+     * MagType, and colorRating.
+     * @param that EarthquakeT object comparing to.
+     * @return boolean true if these two objects are considered equal.
+     */
     public boolean equals(EarthquakeT that) {
     	final double TOLERANCE = 0.0000001;
     	int cmp = date.compareTo(that.getDate());
