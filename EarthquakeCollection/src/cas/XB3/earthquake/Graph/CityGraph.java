2809a1e0897bd88047de0823651bd54fdef5cb59
Fang Ye <yef10@mcmaster.ca>
2020-04-07 15:10:55 -0400

    make CityGrahp essential

--- EarthquakeCollection/src/cas/XB3/earthquake/Graph/CityGraph.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/Graph/CityGraph.java
@@ -1,5 +1,5 @@
 /**
- * Undirected graph data type implemented using a symbol 
+ * Directed graph data type implemented using a HashMap
  * @author Ye Fang
  * Revised: March 24, 2020
  */
@@ -8,12 +8,10 @@ package cas.XB3.earthquake.Graph;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-
-
 public class CityGraph {
-	//HashMap: key = string vertex, value = list of neighboring vertices
+	// HashMap: key = string vertex, value = list of neighboring vertices
 	private HashMap<String, LinkedList<Edge>> adj;
-	//number of edges
+	// number of edges
 	private int E;
 
 	/**
@@ -24,46 +22,27 @@ public class CityGraph {
 	}
 
 	/**
-	 * Adds the edge v-w to this graph
-	 * @param v one vertex in the edge
-	 * @param w another vertex in the edge
+	 * Adds the edge e to this graph
+	 * 
+	 * @param e the Edge object that needs to be added to this graph
 	 */
 	public void addEdge(Edge e) {
-		if(adj.get(e.from()) == null) {
+		if (adj.get(e.from()) == null) {
 			adj.put(e.from(), new LinkedList<>());
 		}
-		if(!adj.get(e.from()).contains(e)) {
+		if (!adj.get(e.from()).contains(e)) {
 			adj.get(e.from()).add(e);
 			E++;
 		}
 	}
-	
+
 	/**
-	 * Gets the list of vertices adjacent to v in this graph
+	 * Gets the list of edges adjacent to v in this graph
+	 * 
 	 * @param v the given vertex
-	 * @return the list of vertices adjacent to vertex v
+	 * @return the list of edges adjacent to vertex v
 	 */
-	public Iterable<Edge> adj(String v){
+	public Iterable<Edge> adj(String v) {
 		return adj.get(v);
-	}
-	
-	/**
-	 * Gets the number of edges in this graph
-	 * @return the number of edges in this graph
-	 */
-	public int getNumberOfEdges() {
-		return this.E;
-	}
-	
-	/**
-	 * Gets the number of vertices in this graph
-	 * @return the number of vertices in this graph
-	 */
-	public int V() {
-		return this.adj.size();
-	}
-	
-	public HashMap<String, LinkedList<Edge>> edges(){
-		return this.adj;
 	}
 }
