1e83c0035c55eb644d458f1fe2c008938ddaf2e1
Fang Ye <yef10@mcmaster.ca>
2020-04-07 00:41:51 -0400

    change the experiment location

--- EarthquakeCollection/src/cas/XB3/earthquake/search/ExperimentsSearch.java
+++ EarthquakeCollection/src/cas/XB3/earthquake/search/ExperimentsSearch.java
@@ -7,17 +7,17 @@ import cas.XB3.earthquake.ADT.PointT;
 import cas.XB3.earthquake.reader.CSVreader;
 
 
-public class TestSearchTime {
+public class ExperimentsSearch {
 	public static void main(String[] args) {
 
 		EarthquakeBag<EarthquakeT> Earthquakebag = new EarthquakeBag<EarthquakeT>();
 		RedBlackBST<Double, EarthquakeT> earthquakeTree = new RedBlackBST<Double, EarthquakeT>();
 		CSVreader.readEarthquakes("./eqarchive-en.csv", Earthquakebag);
 		CSVreader.readEarthquakesBST("./eqarchive-en.csv", earthquakeTree);
-		PointT location = new PointT(60, -95);
+		PointT location = new PointT(46, -70);
 
 
-		int[] radius = { 10, 100, 1000, 4000};
+		int[] radius = { 10, 100, 200, 300,1000, 4000};
 		
 		System.out.println("execution time of earthquake bag");
 		for (int i = 0; i < radius.length; i++) {
